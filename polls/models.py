ec73068a54e552acca5dce0da9272fd0b4ceed67
Yasin Khan <yasinkhan500@gmail.com>
2024-07-16 13:25:34 -0700

    step 7

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
+
 import datetime
 
 
@@ -10,8 +12,14 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
